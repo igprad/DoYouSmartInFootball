550f8a1df9d17bf91464d1032902dfba241da52d
igprad <ign.aldi.pradana@gmail.com>
2017-02-20 19:44:13 +0700

    Updated UI for home and else

--- app/src/main/java/com/android/alz/doyousmartinfootball/Home.java
+++ app/src/main/java/com/android/alz/doyousmartinfootball/Home.java
@@ -2,12 +2,14 @@ package com.android.alz.doyousmartinfootball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
     }
 }
