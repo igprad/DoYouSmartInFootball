e74639b4d05bac34f5d2432106f42e6929497ce6
igprad <ign.aldi.pradana@gmail.com>
2017-02-21 01:40:09 +0700

    Change conscep for Information, root filters from competitions. Already added id for newest competitions in interface FootballData and make the return json in Information activity

--- app/src/main/java/com/android/alz/doyousmartinfootball/InformationHome.java
+++ app/src/main/java/com/android/alz/doyousmartinfootball/InformationHome.java
@@ -1,27 +1,87 @@
 package com.android.alz.doyousmartinfootball;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.alz.doyousmartinfootball.api.FootballData;
+import com.android.alz.doyousmartinfootball.controller.ApiController;
 
 import ru.katso.livebutton.LiveButton;
 
 public class InformationHome extends AppCompatActivity {
-//    private Spinner spinner;
-    private LiveButton btnPlayers,btnTeams,btnCompetitions,btnLeagues;
+    private Spinner spinner;
+    private LiveButton btnCompetitions,btnOK;
+    private ArrayAdapter<String> stringArrayAdapter;
+    private ApiController apiController = new ApiController();
+    private TextView txtView1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_home);
-//        spinner = (Spinner) findViewById(R.id.spinner);
-        btnPlayers = (LiveButton) findViewById(R.id.btnPlayers);
-        btnPlayers = (LiveButton) findViewById(R.id.btnPlayers);
-        btnPlayers = (LiveButton) findViewById(R.id.btnPlayers);
-        btnPlayers = (LiveButton) findViewById(R.id.btnPlayers);
-//        ArrayAdapter<String> stringArrayAdapter
-//                = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
-//                getResources().getStringArray(R.array.array_category));
-//        spinner.setAdapter(stringArrayAdapter);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        btnCompetitions = (LiveButton) findViewById(R.id.btnCompetitions);
+        btnOK = (LiveButton) findViewById(R.id.btnOK);
+        txtView1 = (TextView) findViewById(R.id.txtData);
+        setAdapterSpinnerCategory();
+
+        btnCompetitions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAdapterSpinnerCategory();
+            }
+        });
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(spinner.getSelectedItem().toString().equalsIgnoreCase("Competitions")){
+                    //Untuk data competitions
+                    new GetData(FootballData.COMPETITIONS).execute();
+                }
+            }
+        });
     }
+
+    public void setAdapterSpinnerCategory(){
+        stringArrayAdapter
+                = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.array_for_competitions));
+        spinner.setAdapter(stringArrayAdapter);
+    }
+
+    private class GetData extends AsyncTask<Void,Void,Void> {
+        private String resource,result ;
+
+        public GetData(String resourceInput){resource=resourceInput;}
+
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(InformationHome.this,"Json Data is " +
+                    "downloading",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            result = apiController.getStringJSON(resource);
+            Log.e("Result JSON :",result);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            txtView1.setText(result);
+            Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
--- app/src/main/java/com/android/alz/doyousmartinfootball/api/FootballData.java
+++ app/src/main/java/com/android/alz/doyousmartinfootball/api/FootballData.java
@@ -8,9 +8,22 @@ public interface FootballData {
     String END_POINT = "http://api.football-data.org/v1/";
     String KEY = "7f08807927df41c4b32ea4a986ad764c";
     String COMPETITIONS = "competitions/";
-    String PREMIERE_LEAGUE = "426/";
-    String MANCHESTER_UNITED = "66";
+    String COMP_EUROPEAN_CHAMPIONSHIPS_FRANCE="424/";
+    String COMP_PREMIERE_LEAGUE = "426/";
+    String COMP_CHAMPIONSHIP = "427/";
+    String COMP_LEAGUE_ONE="428/";
+    String COMP_BUNDESLIGA_1="430/";
+    String COMP_BUNDESLIGA_2="431/";
+    String COMP_DFB_POKAL="432/";
+    String COMP_ERIDIVISE="433/";
+    String COMP_LIGUE_1="434/";
+    String COMP_LIGUE_2="435/";
+    String COMP_PRIMERA_DIVSION="436/";
+    String COMP_ADELANTE_LIGA="437/";
+    String COMP_SERIE_A="438/";
+    String COMP_PRIMEIRA_LIGA="439/";
+    String COMP_CHAMPIONS_LEAGUE="440/";
+
     String TEAMS  ="teams/";
-
-
+    String MANCHESTER_UNITED = "66";
 }
